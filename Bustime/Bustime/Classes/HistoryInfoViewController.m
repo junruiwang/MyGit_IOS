69b92cd3701626532b0539c5aa6ba4168a27e872
jerry <junruiwang@gmail.com>
2013-04-08 17:50:56 +0800

    modify file

--- Bustime/Bustime/Classes/HistoryInfoViewController.m
+++ Bustime/Bustime/Classes/HistoryInfoViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "HistoryInfoViewController.h"
+#import "PrettyNavigationBar.h"
 
 @interface HistoryInfoViewController ()
 
@@ -27,6 +28,10 @@
     [super viewDidLoad];
     self.trackedViewName = @"我的收藏页面";
 	self.navigationItem.title = @"我的收藏";
+    [self loadSegmentedButton];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"编辑" style:UIBarButtonItemStyleBordered target:self action:@selector(modifyTable)];
+    
 	[self loadCustomBanner];
 }
 
@@ -36,10 +41,46 @@
     [self customizeNavBar];
 }
 
+- (void)loadSegmentedButton
+{
+    
+    UINavigationBar *navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+    // segmented control as the custom title view
+	NSArray *segmentTextContent = [NSArray arrayWithObjects:
+                                   NSLocalizedString(@"收藏线路", @""),
+                                   NSLocalizedString(@"收藏站点", @""),
+								   nil];
+	UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:segmentTextContent];
+	segmentedControl.selectedSegmentIndex = 0;
+	segmentedControl.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+	segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
+	segmentedControl.frame = CGRectMake(100, 5, 140, kCustomButtonHeight);
+	[segmentedControl addTarget:self action:@selector(segmentAction:) forControlEvents:UIControlEventValueChanged];
+        
+    [navBar addSubview:segmentedControl];
+    UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+    
+    [topView addSubview:navBar];
+    [self.view addSubview:topView];
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)segmentAction:(id)sender
+{
+	// The segmented control was clicked, handle it here
+	//UISegmentedControl *segmentedControl = (UISegmentedControl *)sender;
+    //[segmentedControl.selectedSegmentIndex];
+}
+
+- (void)modifyTable
+{
+    
 }
 
 @end
