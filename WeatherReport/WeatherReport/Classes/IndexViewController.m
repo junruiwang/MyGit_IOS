22f4ba51fc7bad459d1c4fca2fec5d1b51b18904
jerry <junruiwang@gmail.com>
2013-05-16 22:30:14 +0800

    add bar

--- WeatherReport/WeatherReport/Classes/IndexViewController.m
+++ WeatherReport/WeatherReport/Classes/IndexViewController.m
@@ -47,9 +47,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    // 背景设置为黑色
+    self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.000 green:0.000 blue:0.000 alpha:1.000];
+    // 透明度设置为0.3
+    self.navigationController.navigationBar.alpha = 0.300;
+    // 设置为半透明
+    self.navigationController.navigationBar.translucent = YES;
+    
     //加载背景图片
     [self loadBgImageView];
-    [self initalToolbar];
+    //[self initalToolbar];
     //新线程下载一周天气
     [NSThread detachNewThreadSelector:@selector(downloadData) toTarget:self withObject:nil];
 }
@@ -62,7 +69,7 @@
 
 - (void)loadBgImageView
 {
-    float imageHeight = 367;
+    float imageHeight = 412;
     if ([[NSUserDefaults standardUserDefaults] boolForKey:USER_DEVICE_5]) {
         imageHeight += 88;
     }
@@ -70,6 +77,44 @@
     bgImageView.backgroundColor = [UIColor clearColor];
     bgImageView.image = [UIImage imageNamed:@"load_bg1.png"];
     [self.view addSubview:bgImageView];
+    
+    UIView *navigationBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 20, 320, 44)];
+    navigationBarView.backgroundColor = [UIColor clearColor];
+    
+    UIButton *cityButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    cityButton.frame = CGRectMake(10, 10, 25, 22);
+    [cityButton setImage:[UIImage imageNamed:@"country-field.png"] forState:UIControlStateNormal];
+    [navigationBarView addSubview:cityButton];
+    
+    UIImageView *pixImageView = [[UIImageView alloc] initWithFrame:CGRectMake(45, 10, 1, 24)];
+    pixImageView.image = [UIImage imageNamed:@"chooseforard_division_shu.png"];
+    [navigationBarView addSubview:pixImageView];
+    
+    UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(140, 10, 40, 20)];
+    cityLabel.backgroundColor = [UIColor clearColor];
+    cityLabel.font = [UIFont boldSystemFontOfSize:20];
+    cityLabel.textColor = [UIColor whiteColor];
+    cityLabel.textAlignment = NSTextAlignmentCenter;
+    
+    cityLabel.text = @"上海";
+    [navigationBarView addSubview:cityLabel];
+    
+    
+    UIButton *refreshButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    refreshButton.frame = CGRectMake(235, 4, 38, 36);
+    [refreshButton setImage:[UIImage imageNamed:@"barbutton-refresh.png"] forState:UIControlStateNormal];
+    [navigationBarView addSubview:refreshButton];
+    
+    UIImageView *pixImageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(276, 10, 1, 24)];
+    pixImageView2.image = [UIImage imageNamed:@"chooseforard_division_shu.png"];
+    [navigationBarView addSubview:pixImageView2];
+    
+    UIButton *shareButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    shareButton.frame = CGRectMake(280, 4, 36, 36);
+    [shareButton setImage:[UIImage imageNamed:@"forecast-forward-btn.png"] forState:UIControlStateNormal];
+    [navigationBarView addSubview:shareButton];
+    
+    [self.navigationController.view addSubview:navigationBarView];
 }
 
 //初始化工具条
