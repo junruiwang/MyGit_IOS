e30f12f707cd2bfc66b22498d375ca277348f0b3
junruiwang <mac@wangweihua-pc.handpay.com.cn>
2014-12-10 15:04:16 +0800

    add AutoLayout demo

---
+++ AutoLayoutDemo/AutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutDemo
+//
+//  Created by jrwang on 14-12-10.
+//  Copyright (c) 2014年 handpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutDemo/AutoLayoutDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutDemo
+//
+//  Created by jrwang on 14-12-10.
+//  Copyright (c) 2014年 handpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
