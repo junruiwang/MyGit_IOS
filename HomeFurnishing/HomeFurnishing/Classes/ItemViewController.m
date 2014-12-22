6ee5bc926c7ceb8d6f538f84e1da0800fa39a732
junruiwang <jerry@JerryMacBook-Pro.local>
2014-12-22 23:31:44 +0800

    add push icon view

--- HomeFurnishing/HomeFurnishing/Classes/ItemViewController.m
+++ HomeFurnishing/HomeFurnishing/Classes/ItemViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ItemViewController.h"
+#import "Constants.h"
 
 @interface ItemViewController ()
 
+@property(nonatomic, strong) UIView *overlayView;
+@property(nonatomic, strong) UIScrollView *iconView;
+@property(nonatomic, strong) UIScrollView *modelsView;
+@property(nonatomic, strong) NSMutableArray *btnIconList;
 
 @end
 
@@ -17,7 +22,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    [self loadIndicateView];
+    self.btnIconList = [NSMutableArray arrayWithObjects:@"aircona.png", @"curtain.png", @"curtaina.png", @"dinner.png", @"entertainment.png", @"fitting.png", @"gym.png", @"home_arming.png", @"home_at.png", @"home_away.png", @"home_disarm.png", @"home_midnight.png", @"makeup.png", @"meeting.png", @"party.png", @"playground.png", @"pray.png", @"romance.png", @"shower.png", @"silent.png", @"sleeping_day.png", @"sleeping_night.png", @"study.png", @"summer.png", @"winter.png", nil];
+    
+    if (!self.overlayView)
+    {
+        self.overlayView = [[UIView alloc] initWithFrame:self.view.bounds];
+        self.overlayView.backgroundColor = [UIColor blackColor];
+        self.overlayView.alpha = 0.7;
+        self.overlayView.autoresizesSubviews = YES;
+        self.overlayView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleBottomMargin;
+        [self.view addSubview:self.overlayView];
+    }
+    self.iconView = [[UIScrollView alloc] init];
+    self.iconView.backgroundColor = COLOR(234,237,250);
+    
+    self.iconView.layer.cornerRadius = 5;
+    self.iconView.layer.borderColor = [UIColor colorWithRed:215.0/255.0 green:215.0/255.0 blue:215.0/255.0 alpha:1.0].CGColor;
+    self.iconView.layer.borderWidth = 1.0;
+    
+    [self.iconView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.view addSubview:self.iconView];
+    
+    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.iconView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0f constant:240.0f];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.iconView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:00.0f];
+    [self.view addConstraint:constraint];
+    
+    [self.iconView addConstraint:[NSLayoutConstraint constraintWithItem:self.iconView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:600.0f]];
+    
+    [self.iconView addConstraint:[NSLayoutConstraint constraintWithItem:self.iconView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:300.0f]];
+    self.iconView.contentSize = CGSizeMake(600, 500);
+    self.iconView.bounces = NO;
+    self.iconView.directionalLockEnabled = YES;
+    self.iconView.showsHorizontalScrollIndicator = NO;
+    self.iconView.showsVerticalScrollIndicator = YES;
+    
+    
+    NSInteger startX = 23;
+    NSInteger startY = 20;
+    NSInteger xOff = 25;
+    NSInteger yOff = 25;
+    for (NSInteger i=0; i < self.btnIconList.count; i++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        if (i == 0) {
+            
+        }else if (i % 6 != 0){
+            startX += (72 + xOff);
+        }else{
+            startY += (72 + yOff);
+            startX = 23;
+        }
+        
+        btn.frame = CGRectMake(startX, startY, 72, 72);
+        [btn setImage:[UIImage imageNamed:self.btnIconList[i]] forState:UIControlStateNormal];
+        [btn setImage:[UIImage imageNamed:self.btnIconList[i]] forState:UIControlStateHighlighted];
+        [self.iconView addSubview:btn];
+    }
+    
     // Do any additional setup after loading the view.
 }
 
@@ -101,6 +163,7 @@
         default:
             break;
     }
+    self.overlayView.frame = self.view.frame;
 }
 
 @end
